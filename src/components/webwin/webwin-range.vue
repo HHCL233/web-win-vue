d7c461345c277a7efb4e8b873ed3e53433aa8cfc
HHCL233 <XINGTAISHIJIAOXIQU@outlook.com>
2025-10-02 14:59:46 +0800

    feat: 支持自定义滑块主题色

--- src/components/webwin/webwin-range.vue
+++ src/components/webwin/webwin-range.vue
@@ -1,28 +1,37 @@
 <template>
-     <input type="range" class="slider" min="1" max="100" value="50" @change="console.log">
+     <input type="range" class="webwin-slider" min="1" max="100" value="50">
 </template>
 <style scoped>
-input {
-  --c: rgb(98, 0, 255);
+.webwin-slider {
   --l: 2px;
   --s: 20px;
   
+
   width: 600px;
-  height: var(--s); /* needed for Firefox*/
   appearance :none;
   background: none;
-  cursor: pointer;
+  cursor: default;
   overflow: hidden;
 }
 
-input[type="range" i]::-webkit-slider-thumb{
+.webwin-slider[type="range" i]::-webkit-slider-thumb{
   height: var(--s);
   aspect-ratio: 1;
   border-radius: 50%;
-  box-shadow: 0 0 0 3px inset var(--c);
-  border-image: linear-gradient(90deg,var(--c) 50%,#ababab 0) 0 1/calc(50% - 1px) 100vw/0 calc(100vw + 0px);
+  box-shadow: 0 0 0 3px inset v-bind(color);
+  border-image: linear-gradient(90deg,v-bind(color) 50%,#ababab 0) 0 1/calc(50% - 1px) 100vw/0 calc(100vw + 0px);
   -webkit-appearance: none;
   appearance:none;
 }
 
 </style>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  color: { type:String, default: '#006fd0' },
+})
+
+
+</script>
